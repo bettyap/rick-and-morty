31e07550315d27df3d768fb14baa5c9b0c8b9c2c
Elisabeth Aparecida <elisabeth.silva@carambola.com.vc>
2023-04-05 11:17:50 -0300

    feat: create config axios

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,43 @@
-<script setup>
-
-</script>
-
 <template>
-  
+  <h1>personagens</h1>
+  <div>
+    <input type="text" v-model="searchName">
+    <button @click="search">Pesquisar</button>
+  </div>
+  <ul>
+    <li v-for="character in characters" :key="character.id">
+      {{character.name}}
+      <img :src="character.image" alt="" >
+    </li>
+  </ul>
 </template>
+
+<script>
+import api from './services/api'
+
+export default {
+  data() {
+    return {
+      searchName: "",
+      characters: []
+    }
+  },
+  mounted() {
+    api.get('/character').then(response => {
+      this.characters = response.data.results
+      console.log(response.data.results)
+    })
+  },
+  methods: {
+    search() {
+      api.get(`/character?name=${this.searchName}`).then(response => {
+        this.characters = response.data.results
+        console.log(response.data.results)
+      })
+    }
+  }
+}  
+</script>
 
 <style scoped>
 
